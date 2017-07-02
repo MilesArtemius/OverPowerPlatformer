feddc57ac67c9106fe5b2b7ade5cf84b094f7252
milesartemius <miles.tyoma@yandex.ru>
2017-07-02 15:12:56 +0300

    Version 0.1.2; redisign;

--- src/main/java/classes/MainAndMenu/MainController.java
+++ src/main/java/classes/MainAndMenu/MainController.java
@@ -1,33 +1,45 @@
 package classes.MainAndMenu;
 
-import classes.ResizableCanvas;
-import classes.StructureClasses.Level;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.net.URI;
 
 
 public class MainController {
 
-    ResizableCanvas gc = new ResizableCanvas();
-    Level level;
+    @FXML
+    private VBox vBox;
 
     @FXML
-    private AnchorPane anchorPane;
+    private TilePane pane;
 
     @FXML
-    private Button button;
+    private Button new_game_button;
 
-    private void setScene() {
+    @FXML
+    private Button continue_button;
+
+    @FXML
+    private Button something_else_button;
+
+    @FXML
+    private Hyperlink link;
+
+    private void continueGame() {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("/layouts/game_layout.fxml"));
 
-            Stage stage = (Stage) anchorPane.getScene().getWindow();
+            Stage stage = (Stage) pane.getScene().getWindow();
 
             System.out.println(stage);
 
@@ -35,43 +47,33 @@ public class MainController {
 
             stage.setScene(scene);
 
-            //gc.start();
-
-            /*scene.addEventHandler(KeyEvent.ANY, keyEvent -> {
-                if (keyEvent.getEventType() == KeyEvent.KEY_PRESSED) {
-                    switch (keyEvent.getCode()) {
-                        case UP:
-                            gc.jump(1);
-                            break;
-                        case RIGHT:
-                            gc.move(1);
-                            break;
-                        case LEFT:
-                            gc.move(2);
-                            break;
-                    }
-                } else {
-                    switch (keyEvent.getCode() ) {
-                        case RIGHT:
-                        case LEFT:
-                            gc.move(0);
-                            break;
-                        case UP:
-                            gc.jump(0);
-                            break;
-                    }
-                }
-            });*/
-
-            //stage.setFullScreen(true);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    private void startNewGame() {}
+
+    private void somethingElse() {}
+
     @FXML
     public void initialize(){
-        button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> setScene());
+        pane.setBackground(new Background(new BackgroundImage(new Image(getClass().getResource("/textures/ErEsursE.png").toString()),
+                null, null, null,
+                new BackgroundSize(1, 1, true, true, false, false))));
+
+        vBox.spacingProperty().bind(pane.heightProperty().divide(vBox.getChildren().size() * 2));
+
+        new_game_button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> startNewGame());
+        continue_button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> continueGame());
+        something_else_button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> somethingElse());
+        link.setOnAction(event -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://firebase.google.com/"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
     }
 }
