ac3f1fad53ec00e68bda486dbff54cae3da783c6
milesartemius <miles.tyoma@yandex.ru>
2017-07-05 16:58:30 +0300

    Version 0.1.3; redactor;

--- src/main/java/classes/LevelEditor/EditorController.java
+++ src/main/java/classes/LevelEditor/EditorController.java
@@ -2,26 +2,27 @@ package classes.LevelEditor;
 
 import classes.ResizableCanvas;
 import classes.StructureClasses.TreeFile;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import static classes.MainAndMenu.Depacker.pathname;
 
@@ -35,6 +36,9 @@ public class EditorController {
     File level;
     File config;
     File blockset;
+
+    TreeFile openedInConsoleFile;
+    PrintWriter consolePrintWriter;
 
     @FXML
     private BorderPane rootPane;
@@ -68,9 +72,9 @@ public class EditorController {
     private TreeItem<TreeFile> fileTreeBuilder(TreeFile file, boolean deep) {
         TreeItem<TreeFile> treeItem = new TreeItem<>(file);
         if (file.isDirectory()) {
-            for (File subfile: file.listFiles())  {
+            for (File subfile: file.listFiles()) {
                 TreeFile subtreefile = new TreeFile(subfile.getAbsolutePath(), subfile.getAbsolutePath().substring(subfile.getAbsolutePath().lastIndexOf('\\') + 1));
-                treeItem.getChildren().add(((deep)?(fileTreeBuilder(subtreefile, true)):(new TreeItem<>(subtreefile))));
+                treeItem.getChildren().add(((deep) ? (fileTreeBuilder(subtreefile, true)) : (new TreeItem<>(subtreefile))));
             }
         } else {
             return new TreeItem<>(file);
@@ -100,15 +104,55 @@ public class EditorController {
         levelView.heightProperty().bind(levelViewContainer.heightProperty());
 
         folderView.setOnMouseClicked(event -> {
-            if (event.getClickCount() > 1) {
+            if (event.getButton() == MouseButton.SECONDARY) {
+                try {
+                    TreeItem treeItem = (TreeItem) folderView.getSelectionModel().getSelectedItem();
+                    TreeFile treeFile = (TreeFile) treeItem.getValue();
+                    if (((treeFile.isFile()) && (treeFile.getName().substring(treeFile.getName().lastIndexOf('.')).equals(".upson"))) || (folderView.getRoot().equals(treeItem))) {
+                        ContextMenu contextMenu = new ContextMenu();
+                        MenuItem menuItem = new MenuItem("Rename");
+                        menuItem.setOnAction(event1 -> {
+                            try {
+                                TextInputDialog renaming = new TextInputDialog(folderView.getRoot().getValue().toString());
+                                renaming.showAndWait();
+                                Files.move(Paths.get(treeFile.getAbsolutePath()), Paths.get(treeFile.getAbsolutePath()).resolveSibling(renaming.getResult()));
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+                        contextMenu.getItems().add(menuItem);
+                        contextMenu.show(rootPane.getScene().getWindow(), event.getScreenX(), event.getScreenY());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (event.getClickCount() > 1) {
                 TreeItem treeItem = (TreeItem) folderView.getSelectionModel().getSelectedItem();
                 TreeFile treeFile = (TreeFile) treeItem.getValue();
                 try {
                     if (treeFile.isFile()) {
-                        System.out.println(new String(Files.readAllBytes(Paths.get(treeFile.getAbsolutePath())), Charset.defaultCharset()));
+                        openedInConsoleFile = treeFile;
                         consoleView.setText(new String(Files.readAllBytes(Paths.get(treeFile.getAbsolutePath()))));
+                        if (consoleView.getText().length() == 0) {
+                            consoleView.setPromptText("Press 'Ctrl + Enter' to save changes");
+                        }
+                    } else {
+                        consoleView.setText("");
+                        consoleView.setPromptText("Press 'Ctrl + Enter' to save changes");
                     }
                 } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        consoleView.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if ((event.getCode() == KeyCode.ENTER) && (consoleView.isFocused()) && (consoleView.getText() != null) && (event.isControlDown())) {
+                try {
+                    consolePrintWriter = new PrintWriter(openedInConsoleFile.getAbsolutePath());
+                    consolePrintWriter.println(consoleView.getText());
+                    consolePrintWriter.flush();
+                } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
             }
--- src/main/java/classes/LevelEditor/SinglelineDialigController.java
+++ src/main/java/classes/LevelEditor/SinglelineDialigController.java
@@ -12,5 +12,6 @@ public class SinglelineDialigController {
 
     @FXML
     public void initialize() {
+
     }
 }
