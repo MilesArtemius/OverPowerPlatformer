1fa41c9c7e3b387aaf494ba11cc550bf9842c894
milesartemius <miles.tyoma@yandex.ru>
2017-08-25 04:11:33 +0300

    Version: 2.1; Engine division;

---
+++ src/main/java/greensun/launcher/MainController.java
@@ -0,0 +1,144 @@
+package greensun.launcher;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+public class MainController {
+
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    private TilePane pane;
+
+    @FXML
+    private NeoButton new_game_button;
+
+    @FXML
+    private NeoButton continue_button;
+
+    @FXML
+    private NeoButton redactor_button;
+
+    @FXML
+    private Hyperlink link;
+
+    private void continueGame() {
+        try {
+
+            //scrrenDownButton(continue_button, "C:\\OP_GAME_SYS\\games\\continueButton.png");
+
+            Parent root = FXMLLoader.load(getClass().getResource("/layouts/game_layout.fxml"));
+
+            Stage stage = (Stage) pane.getScene().getWindow();
+
+            Scene scene = new Scene(root, stage.getWidth(), stage.getHeight());
+
+            stage.setScene(scene);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startNewGame() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/layouts/level_chosen_layout.fxml"));
+
+            Stage stage = (Stage) pane.getScene().getWindow();
+
+            Scene scene = new Scene(root, stage.getWidth(), stage.getHeight());
+
+            stage.setScene(scene);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void somethingElse() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/layouts/editor_layout.fxml"));
+
+            Stage stage = new Stage();//
+
+            Scene scene = new Scene(root, pane.getScene().getWindow().getWidth(), pane.getScene().getWindow().getHeight());
+
+            stage.setScene(scene);
+            stage.setTitle("Ultimate Level Editor");
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize(){
+        pane.setBackground(new Background(new BackgroundImage(new Image(getClass().getResource("/utils/pictures/TheGreenSun.jpg").toString()),
+                null, null, null,
+                new BackgroundSize(1, 1, true, true, false, false))));
+
+        vBox.spacingProperty().bind(pane.heightProperty().divide(vBox.getChildren().size() * 2));
+
+        new_game_button.setHeight(8); //64 for screening;
+        new_game_button.setText("NEW GAME");
+        new_game_button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> startNewGame());
+
+        continue_button.setHeight(8);
+        continue_button.setText("CONTINUE");
+        continue_button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> continueGame());
+
+        redactor_button.setHeight(8);
+        redactor_button.setText("REDACTOR");
+        redactor_button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> somethingElse());
+        /*link.setOnAction(event -> {
+            try {
+                Desktop.getDesktop().browse(new URI("https://firebase.google.com/"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });*/
+    }
+
+    public void scrrenDownButton(NeoButton button, String pathToFile) {
+        WritableImage image = button.snapshot(null, null);
+        try {
+            File file = new File(pathToFile);
+            BufferedImage bImage = SwingFXUtils.fromFXImage(image, null);
+            //System.out.println(n.getHeight());
+            ImageIO.write(
+                    bImage,
+                    "png",
+                    file);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+
+/*<StackPane prefHeight="150.0" prefWidth="200.0">
+            <children>
+                <Text text="Text">
+                    <font>
+                        <Font name="Veranda" size="24.0" />
+                    </font>
+                </Text>
+            </children>
+         </StackPane>*/
---
+++ src/main/java/greensun/redactor/TreeFile.java
@@ -0,0 +1,71 @@
+package greensun.redactor;
+
+import com.google.common.io.Files;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by HP on 04.07.2017.
+ */
+public class TreeFile extends File {
+    String viewName;
+
+    public TreeFile(String pathname) {
+        super(pathname);
+        this.viewName = "NoName";
+    }
+
+    @Override
+    public String toString() {
+        return viewName;
+    }
+
+    public TreeFile(String pathname, String viewName) {
+        super(pathname);
+        this.viewName = viewName;
+    }
+
+    public boolean isLevel() {
+        try {
+            if ((this.canRead()) && (this.isDirectory()) && (this.listFiles().length > 0)) {
+                ArrayList<String> files = new ArrayList<>();
+                boolean levelCounter = false;
+                for (File file : this.listFiles()) {
+                    files.add(file.getName());
+                    if ((file.isDirectory()) && (file.getName().equals("levels"))) {
+                        for (File level : file.listFiles()) {
+                            if (Files.getFileExtension(level.getName()).equals("upson")) {
+                                levelCounter = true;
+                            }
+                        }
+                    }
+                }
+                if ((files.contains("levels")) &&
+                        (files.contains("textures")) &&
+                        (files.contains("blockset.json")) &&
+                        (files.contains("config.json")) &&
+                        levelCounter) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+            return false;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    public boolean isImage() {
+        if ((this.isFile()) && (Files.getFileExtension(this.getName()).equals("png"))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setViewName(String name) {
+        this.viewName = name;
+    }
+}
